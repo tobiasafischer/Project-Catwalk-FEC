c947bcbf58fa46065fb8f1bd42a95a436296da58
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-14 16:15:59 -0700

    Refactored for hooks

--- client/src/components/R-and-R/components/reviews-list.jsx
+++ client/src/components/R-and-R/components/reviews-list.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import ReviewTile from './review-tile.jsx';
+import { React, useState } from 'react';
+import ReviewTile from './review-tile';
 
-
-class ReviewsList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tilesDisplayed: 2,
-      moreThanTwo: false,
-      reviewsLeft: null,
-    };
+// tiles will be [{stars}, {}]
+function renderTiles(tiles) {
+  const arrOfTiles = [];
+  for (let i = 0; i < tiles.length; i += 1) {
+    arrOfTiles.push(
+      <ReviewTile stars={tiles.stars} />,
+    );
   }
+  return arrOfTiles;
+}
 
-  render() {
-    return (
-      <div className="reviews-list">
-        <h2>ReviewList</h2>
-        <ReviewTile stars={1.252525252}/>
-      </div>
-      
-    )
-  }
+function ReviewsList() {
+  // const [tilesDisplayed, setTilesDisplayed] = useState(2);
+  // const [reviews, setReviews] = useState(() => getReviews());
+  const [tiles] = useState([{ stars: 2 }, { stars: 2 }]);
+  return (
+    <div className="reviews-list">
+      <h2>ReviewList</h2>
+      {renderTiles(tiles)}
+    </div>
+  );
 }
 
 export default ReviewsList;
