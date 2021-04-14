99167bdf557954e5c8b625c2bab4f59aa327d197
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-14 11:22:07 -0700

    Set up barebones for r&r

---
+++ client/src/components/R&R/r&r.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Review extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  render() {
+    return (
+      <h1>This is our app</h1>
+      
+    )
+  }
+}
+
+export default Review;
