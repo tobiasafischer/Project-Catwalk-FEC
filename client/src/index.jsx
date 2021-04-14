e48dfa5e5a191cd13c0934cc2d029998aa5d7ab4
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-14 15:55:39 -0700

    Refactored index to utilize hooks with functional components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Review from '../../client/src/components/R-and-R/r-and-r.jsx';
+import Review from './components/R-and-R/r-and-r';
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>This is our app</h1>
-        <Review />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <h1>This is our app</h1>
+      <Review />
+    </div>
+  );
 }
 
 ReactDOM.render(
   <App />,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
