257e4928c13045118901f85bc660510c8caee1e9
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-14 16:04:56 -0700

    Refactored for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-var path = require("path");
-var SRC_DIR = path.join(__dirname, "/client/src");
-var DIST_DIR = path.join(__dirname, "/client/dist");
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: DIST_DIR,
   },
   module: {
-      
     rules: [
       {
         test: /\.jsx?/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
-        test: /\.(jpe?g|png|gif|svg)$/i, 
+        test: /\.(jpe?g|png|gif|svg)$/i,
         loader: 'file-loader',
         options: {
           name: '[path][name].[ext]',
         },
-      }
+      },
     ],
   },
 };
