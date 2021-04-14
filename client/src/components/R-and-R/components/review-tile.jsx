f9c5213b7ba1d5d35fdc29278bd7b22bfe34ebdb
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-14 16:21:01 -0700

    Refactored for hooks

--- client/src/components/R-and-R/components/review-tile.jsx
+++ client/src/components/R-and-R/components/review-tile.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import Stars from './stars.jsx';
-class ReviewTile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stars: this.props.stars,
-      starsRounded: Math.floor(this.props.stars * 4)
-    };
-  }
+import { React, useState } from 'react';
+import Stars from './stars';
 
-  render() {
-    return (
-      <div className="review-tile">
-        <h2>Review Tile</h2>
-        {<Stars stop={20} step={4} fractions={4} initialRating={this.state.starsRounded} readonly={true}/>}
-      </div>
-      
-    )
-  }
+function roundStars(star) {
+  return Math.floor(star * 4);
+}
+
+function ReviewTile(props) {
+  // refactor to use ref so it only rerenders on stars change --> memoize the roundStars
+  const [stars] = useState(props);
+  const [starsRounded] = useState(() => roundStars(stars));
+
+  return (
+    <div className="review-tile">
+      <h2>Review Tile</h2>
+      <Stars stop={20} step={4} fractions={4} initialRating={starsRounded} readonly="true" />
+    </div>
+  );
 }
 
 export default ReviewTile;
