9c76537dce3d529e3008265f5d917157a3df7bf7
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-14 17:02:53 -0700

    Added review helpfulness rating system

--- client/src/components/R-and-R/components/review-tile.jsx
+++ client/src/components/R-and-R/components/review-tile.jsx
@@ -9,10 +9,29 @@ function ReviewTile(props) {
   // refactor to use ref so it only rerenders on stars change --> memoize the roundStars
   const [{ stars }] = useState(props);
   const [starsRounded] = useState(() => roundStars(stars));
-  console.log(stars);
+  const [{ title }] = useState(props);
+  const [{ body }] = useState(props);
+  const [positiveReviews, setPositiveReviews] = useState(0);
+  const [reportCount, setReportCount] = useState(0);
   return (
     <div className="review-tile">
       <Stars stop={20} step={4} fractions={4} initialRating={starsRounded} readonly="true" />
+      <div id="review-text-container">
+        <h1>{ title }</h1>
+        <p>{ body }</p>
+        <div id="helpful">
+          <p>Helpful?</p>
+          <div id="helpful-yes" onClick={() => setPositiveReviews(positiveReviews + 1)} aria-hidden="true">
+            Yes (
+            {positiveReviews}
+            )
+          </div>
+          <p>   |   </p>
+          <div id="report" onClick={() => setReportCount(reportCount + 1)} aria-hidden="true">
+            Report
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
