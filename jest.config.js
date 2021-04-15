bb318b1f4fde380850f6446217f15bcf6d5999dd
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-15 16:37:22 -0700

    Got jest tests to render

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.jsx$': 'babel-jest'
