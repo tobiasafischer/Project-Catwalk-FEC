4415f0b78d37d58132dd184c0cfe80e254a2f7c7
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-14 17:15:43 -0700

    Added bootstrap dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
@@ -23,6 +24,34 @@ module.exports = {
         options: {
           name: '[path][name].[ext]',
         },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          // inject CSS to page
+          loader: 'style-loader',
+        }, {
+          // translates CSS into CommonJS modules
+          loader: 'css-loader',
+        }, {
+          // Run postcss actions
+          loader: 'postcss-loader',
+          options: {
+            // `postcssOptions` is needed for postcss 8.x;
+            // if you use postcss 7.x skip the key
+            postcssOptions: {
+              // postcss plugins, can be exported to postcss.config.js
+              plugins() {
+                return [
+                  autoprefixer,
+                ];
+              },
+            },
+          },
+        }, {
+          // compiles Sass to CSS
+          loader: 'sass-loader',
+        }],
       },
     ],
   },
