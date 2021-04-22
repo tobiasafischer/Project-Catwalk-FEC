5d86d78b04658f2aab5ab1597fe7976e150401a6
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-22 13:08:42 -0700

    Got request to work, but no data comes back... need to investigate

--- server/server.js
+++ server/server.js
@@ -4,15 +4,29 @@ const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const API_KEY = require('../config.js');
+const { API_KEY } = require('../config.js');
 
 const apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax43/';
+const reviewHeader = {
+  headers: {
+    Authorization: API_KEY,
+  },
+  params: {
+    product_id: 0,
+  },
+};
+
+const productHeader = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(cors());
-
+app.use(bodyParser.json());
 
 app.listen(3000);
 
@@ -25,9 +39,23 @@ app.post('/', (req, res) => {
 });
 
 app.get('/reviews', (req, res) => {
-  axios.get(`${apiUrl}/reviews/`)
+  axios.get(`${apiUrl}reviews/`, reviewHeader)
     .then((data) => {
-      console.log(data.results);
+      console.log(data.data);
       res.sendStatus(201);
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
+});
+
+app.get('/products', (req, res) => {
+  axios.get(`${apiUrl}products/`, productHeader)
+    .then((data) => {
+      console.log(data.data);
+      res.sendStatus(201);
+    })
+    .catch(() => {
+      res.sendStatus(500);
     });
 });
