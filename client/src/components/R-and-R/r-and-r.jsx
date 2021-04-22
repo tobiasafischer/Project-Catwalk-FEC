30d8ecdf51f88a636f1927d1cbcd9dcea251928e
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-22 12:06:35 -0700

    Installed axios

--- client/src/components/R-and-R/r-and-r.jsx
+++ client/src/components/R-and-R/r-and-r.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReviewsList from './components/reviews-list';
+import axios from 'axios';
 
-const Review = () => (
-  <div className="review">
-    <p>RATINGS & REVIEWS</p>
-    <div id="reviews-list">
-      <ReviewsList
-        stars={[
-          { stars: 1 },
-          { stars: 6 },
-          { stars: 2 },
-          { stars: 4 },
-        ]}
-      />
+const Review = () => {
+  const getReviews = () => {
+
+  };
+  return (
+    <div className="review">
+      <p>RATINGS & REVIEWS</p>
+      <div id="reviews-list">
+        <ReviewsList
+          stars={[
+            { stars: 1 },
+            { stars: 6 },
+            { stars: 2 },
+            { stars: 4 },
+          ]}
+        />
+      </div>
     </div>
-  </div>
-);
+)};
 
 export default Review;
