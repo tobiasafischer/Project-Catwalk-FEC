c8844978263d04d34a4163fa6c1b79978e949b65
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-22 12:15:52 -0700

    Setting up api handles

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
-const path = require("path");
-const express = require("express");
-const apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax43/';
+const path = require('path');
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const API_KEY = require('../config.js');
+
+const apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax43/';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(cors());
+
 
 app.listen(3000);
 
@@ -15,4 +22,12 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   res.sendStatus(201);
+});
+
+app.get('/reviews', (req, res) => {
+  axios.get(`${apiUrl}/reviews/`)
+    .then((data) => {
+      console.log(data.results);
+      res.sendStatus(201);
+    });
 });
