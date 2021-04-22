1d0c6c23a3020a4cd2a3ee9b6cf12970badd4d6b
B1-sauce <hby940604@gmail.com>
2021-04-21 21:04:38 -0700

    implemented thumbnail list

--- client/src/components/overview/imageGalley/ImageGallery.jsx
+++ client/src/components/overview/imageGalley/ImageGallery.jsx
@@ -18,6 +18,7 @@ class ImageGallery extends React.Component {
     }
     this.goToNext = this.goToNext.bind(this);
     this.goToPrevious = this.goToPrevious.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   goToNext() {
     if (this.state.currentInd < this.state.data.length - 1) {
@@ -25,7 +26,7 @@ class ImageGallery extends React.Component {
         currentInd: this.state.currentInd + 1,
         predisplay: 'button'
       }, () => {
-        if (this.state.currentInd = this.state.data.length - 1) {
+        if (this.state.currentInd === this.state.data.length - 1) {
           this.setState({
             nextdisplay: 'hidden'
           })
@@ -47,26 +48,16 @@ class ImageGallery extends React.Component {
       })
     }
   }
-
+  handleClick(e) {
+    this.setState({
+      currentInd: Number(e.target.id)
+    }, () => { console.log(e.target.id) })
+  }
   render() {
     return (
       <div id='img'>
         <Carouse data={this.state.data} currentInd={this.state.currentInd} goToNext={this.goToNext} goToPrevious={this.goToPrevious} predisplay={this.state.predisplay} nextdisplay={this.state.nextdisplay} />
-        {/* <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <Carouse data={this.state.data} />
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-        <button className='expand' onClick={this.props.toggleExpand}><FontAwesomeIcon icon={faExpand} /></button> */}
+        <ThumbnailPic data={this.state.data} handleClick={this.handleClick} />
         <button className='expand' onClick={this.props.toggleExpand}><FontAwesomeIcon icon={faExpand} /></button>
       </div>
     )
--- client/src/components/overview/imageGalley/thubnailpic.jsx
+++ client/src/components/overview/imageGalley/thubnailpic.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const ThumbnailPic = (props) => {
-  return <div>
-    {
-      props.data.map((pic, index) => {
-        return (
-          <img src={pic} className='img-thumbnail' />
-        )
-      })
-    }
-  </div>
-
+  return (
+    <ul id='thumbnail'>
+      {
+        props.data.map((pic, index) => {
+          return (
+            <li id='thumbnail-container' key={index}>
+              <img id={index} src={pic} onClick={props.handleClick} />
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
 }
 export default ThumbnailPic;
