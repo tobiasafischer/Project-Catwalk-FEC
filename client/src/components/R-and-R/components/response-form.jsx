075099f55027b577df2f49a04cbdcc887f28f0c4
tobiasafischer <tobias.alan.fischer@gmail.com>
2021-04-23 14:19:00 -0700

    Revert "Have to refactor all of my form"
    This reverts commit 7a41a4f06d23422e862d250676c5f642b6341316.

--- client/src/components/R-and-R/components/response-form.jsx
+++ client/src/components/R-and-R/components/response-form.jsx
@@ -116,6 +116,114 @@ const ResponseForm = (props) => {
               />
               <ReviewForm />
             </div>
+<<<<<<< HEAD
+=======
+            <Form>
+              <Form.Row>
+                <div style={{ marginTop: '10px' }}>
+                  <Form.Check
+                    type="checkbox"
+                    id="default-checkbox"
+                    label="I recommend this item"
+                    value={recommend}
+                    onChange={() => setRecommend(!recommend)}
+                  />
+                </div>
+              </Form.Row>
+              <Form.Row>
+
+                <Form.Group
+                  as={Col}
+                  controlId="formGridName"
+                >
+                  <Form.Label>
+                    <div style={{ marginTop: '20px' }}>
+                      Name
+                    </div>
+                  </Form.Label>
+                  <Form.Control
+                    placeholder="jackson11!"
+                    value={name}
+                    onChange={(e) => checkChar(e.target.value, 60, setName)}
+                    type="name"
+                  />
+                  <Form.Text id="summaryHelpBlock" muted>
+                    For privacy reasons, do not use your full name or email address
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group
+                  as={Col}
+                  controlId="formGridEmail"
+                >
+                  <Form.Label>
+                    <div style={{ marginTop: '20px' }}>
+                      Email
+                    </div>
+                  </Form.Label>
+                  <Form.Control
+                    placeholder="jackson11@email.com"
+                    value={email}
+                    onChange={(e) => checkChar(e.target.value, 60, setEmail)}
+                    type="email"
+                  />
+                  <Form.Text id="summaryHelpBlock" muted>
+                    For authentication reasons, you will not be emailed
+                  </Form.Text>
+                </Form.Group>
+
+              </Form.Row>
+
+              <Form.Group controlId="formGridSummary">
+                <Form.Label>
+                  <div style={{ marginTop: '20px' }}>
+                    Summary
+                  </div>
+                </Form.Label>
+                <Form.Control
+                  placeholder="Example: Best purchase ever!"
+                  onChange={(e) => checkChar(e.target.value, 60, setSummary)}
+                  value={summary}
+                />
+                <Form.Text id="summaryHelpBlock" muted>
+                  Your summary must be 1-60 characters long.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formGridBody">
+                <Form.Label>
+                  <div style={{ marginTop: '20px' }}>
+                    Review
+                  </div>
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  placeholder="Why did you like the product or not?"
+                  onChange={(e) => checkChar(e.target.value, 1000, setBody)}
+                  value={body}
+                />
+                <Form.Text id="bodyHelpBlock" muted>
+                  {bodyCounter}
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group>
+                <DropzoneAreaBase
+                  acceptedFiles={['image/*']}
+                  filesLimit={5}
+                  fileObjects={unformat}
+                  onAdd={(photo) => {
+                    setPhotos([].concat(photos, photo[0].data));
+                    setUnformat([].concat(unformat, photo));
+                  }}
+                  onDelete={(photo) => setPhotos(photos.filter((item) => item.data !== photo.data))}
+                  dropzoneText="Upload up to (5) images"
+                  showFileNames="true"
+                />
+              </Form.Group>
+
+            </Form>
+>>>>>>> parent of 7a41a4f... Have to refactor all of my form
           </Modal.Body>
           <Modal.Footer>
             <button
